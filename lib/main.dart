2b074f5a29569754b6a183dfdfc663fb586fec3a
lou <dustyfingers01257@gmail.com>
2021-04-18 10:04:05 -0600

    fixed number display

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,58 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
+  Widget buildKey(int keyNumber) {
+    Color keyColor;
+    int shownKeyNumber = keyNumber + 1;
+
+    switch (keyNumber) {
+      case 0:
+        keyColor = Colors.red;
+        break;
+      case 1:
+        keyColor = Colors.orange;
+        break;
+      case 2:
+        keyColor = Colors.yellow;
+        break;
+      case 3:
+        keyColor = Colors.green;
+        break;
+      case 4:
+        keyColor = Colors.blue;
+        break;
+      case 5:
+        keyColor = Colors.indigo;
+        break;
+      case 6:
+        keyColor = Colors.purple;
+        break;
+      default:
+        keyColor = Colors.red;
+    }
+
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(backgroundColor: keyColor),
+        onPressed: () {
+          playSound(shownKeyNumber);
+        },
+        child: Text(
+          'Note $shownKeyNumber',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  List buildKeyboard() {
+    List keyboard = <Widget>[];
+    for (int i = 0; i < 7; i++) {
+      keyboard.add(buildKey(i));
+    }
+    return keyboard;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,92 +69,7 @@ class XylophoneApp extends StatelessWidget {
           child: Center(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.red),
-                    onPressed: () {
-                      playSound(1);
-                    },
-                    child: Text(
-                      'Note 1',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.orange),
-                    onPressed: () {
-                      playSound(2);
-                    },
-                    child: Text(
-                      'Note 2',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.yellow),
-                    onPressed: () {
-                      playSound(3);
-                    },
-                    child: Text(
-                      'Note 3',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.green),
-                    onPressed: () {
-                      playSound(4);
-                    },
-                    child: Text(
-                      'Note 4',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                    onPressed: () {
-                      playSound(5);
-                    },
-                    child: Text(
-                      'Note 5',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.indigo),
-                    onPressed: () {
-                      playSound(6);
-                    },
-                    child: Text(
-                      'Note 6',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    style: TextButton.styleFrom(backgroundColor: Colors.purple),
-                    onPressed: () {
-                      playSound(7);
-                    },
-                    child: Text(
-                      'Note 7',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
+              children: buildKeyboard(),
             ),
           ),
         ),
