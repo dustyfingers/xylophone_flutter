199312a55ee4784ff5094f5c7f958b63ec31ce66
lou <dustyfingers01257@gmail.com>
2021-04-18 10:01:47 -0600

    finished layout of xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -4,134 +4,103 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
-    final player = AudioCache();
-
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style:
-                            TextButton.styleFrom(backgroundColor: Colors.red),
-                        onPressed: () {
-                          player.play('note1.wav');
-                        },
-                        child: Text(
-                          'Note 1',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () {
+                      playSound(1);
+                    },
+                    child: Text(
+                      'Note 1',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.orange),
-                        onPressed: () {
-                          player.play('note2.wav');
-                        },
-                        child: Text(
-                          'Note 2',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                    onPressed: () {
+                      playSound(2);
+                    },
+                    child: Text(
+                      'Note 2',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.yellow),
-                        onPressed: () {
-                          player.play('note3.wav');
-                        },
-                        child: Text(
-                          'Note 3',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                    onPressed: () {
+                      playSound(3);
+                    },
+                    child: Text(
+                      'Note 3',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style:
-                            TextButton.styleFrom(backgroundColor: Colors.green),
-                        onPressed: () {
-                          player.play('note4.wav');
-                        },
-                        child: Text(
-                          'Note 4',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.green),
+                    onPressed: () {
+                      playSound(4);
+                    },
+                    child: Text(
+                      'Note 4',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style:
-                            TextButton.styleFrom(backgroundColor: Colors.blue),
-                        onPressed: () {
-                          player.play('note5.wav');
-                        },
-                        child: Text(
-                          'Note 5',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: () {
+                      playSound(5);
+                    },
+                    child: Text(
+                      'Note 5',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.indigo),
-                        onPressed: () {
-                          player.play('note6.wav');
-                        },
-                        child: Text(
-                          'Note 6',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.indigo),
+                    onPressed: () {
+                      playSound(6);
+                    },
+                    child: Text(
+                      'Note 6',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            backgroundColor: Colors.purple),
-                        onPressed: () {
-                          player.play('note7.wav');
-                        },
-                        child: Text(
-                          'Note 7',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                    onPressed: () {
+                      playSound(7);
+                    },
+                    child: Text(
+                      'Note 7',
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
